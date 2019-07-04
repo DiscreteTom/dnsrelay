7ed0baa826b375d3f038c27a76b676b9baccc135
buptjincheng <1470607814@qq.com>
2019-07-04 18:12:21 +0800

    try5

--- src/data.py
+++ src/data.py
@@ -22,19 +22,17 @@ class Data:
 		self.data = yaml.safe_load(f)
 		f.close()
 	
-	def find(self, url: str) -> str:
+	def find(self, url: str) -> list:
 		'''
 		- if `url` is found, return its ip address
 		- if `url` is '0.0.0.0', return '0.0.0.0'
 		- if `url` is not found, return empty str
 		'''
-		# TODO: 
-		if url == '0.0.0.0' :
-			return '0.0.0.0'
-		elif url in self.data :
+		#TODO: 
+		if url in self.data :
 			return self.data[url]
 		else:
-			return ''
+			return ['']
 
 
 	def add(self, name: str, ttl: int, cls: str, type: str, value: str) -> None:
@@ -42,7 +40,10 @@ class Data:
 		add a record to data file
 		'''
 		#ODO: add record to self.data
-		self.data[name] = value
+		if name in self.data	:
+			self.data[name].append(value)
+		else	:
+			self.data[name] = [value]
 		# save current data to data file
 		f = open(self.filename, 'w', encoding='utf-8')
 		yaml.safe_dump(self.data, f)
